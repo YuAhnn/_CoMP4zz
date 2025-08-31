d5e68de482fa2104e74be9c756beb66b693279cc
YuAhnn <tcc.hermoso.y@gmail.com>
2025-08-31 12:09:52 +0800

    Updating the options hover animation for mobile devices

--- client/src/components/EmojiAssesment.jsx
+++ client/src/components/EmojiAssesment.jsx
@@ -3,6 +3,7 @@ import sad from '../assets/sad.png'
 import angry from '../assets/angry.png'
 import confused from '../assets/confused.png'
 import anxious from '../assets/anxious.png'
+import { useState } from 'react'
 
 const emojiChoices = [
   {option: smile, alt: "Happy"},
@@ -15,18 +16,25 @@ const emojiChoices = [
 const question = "How are you feeling right now?"
 
 const EmojiAssesment = () => {
+  const[selected, setSelected] = useState(null)
+
   return (
     <main className="flex flex-col items-center justify-center h-screen mx-auto max-w-[1000px] gap-10">
       <h1 className="text-xl sm:text-4xl md:text-5xl font-bold md:py-2">{question}</h1>
-      {/* <hr className="font-black w-[600px] border-1"/> */}
       <div className="mx-4">
         <figure className='flex flex-wrap justify-evenly gap-5'>
-           {emojiChoices.map((emoji , index) => (
-            <div key={index}>
-              <img src={emoji.option} alt={emoji.alt} className="w-[100px] md:w-[90px] sm:w-[60px] rounded-2xl grayscale hover:grayscale-0 duration-300 cursor-pointer hover:filter  hover:drop-shadow-[0_0_6px_rgba(0,0,0,0.6)] ease-in-out" />
+          {emojiChoices.map((emoji, index) => (
+            <div key={index} onClick={() => setSelected(index)}>
+              <img
+                src={emoji.option}
+                alt={emoji.alt}
+                className={`w-[100px] md:w-[90px] sm:w-[60px] rounded-2xl grayscale 
+                  duration-300 cursor-pointer ease-in-out
+                  ${selected === index ? 'grayscale-0 drop-shadow-[0_0_6px_rgba(0,0,0,0.6)]' : 'hover:grayscale-0 hover:drop-shadow-[0_0_6px_rgba(0,0,0,0.6)]'}`}
+              />
             </div>
           ))}
-      </figure>
+        </figure>
       </div>
     </main>
   )
